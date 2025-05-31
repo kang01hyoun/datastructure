7e515f3488bb3600c0f67fc36bedd4fc3abbfd25
kang01hyoun <gus7618@gmail.com>
2025-05-31 23:59:08 +0900

    bugfix

--- HeapSORT/main.c
+++ HeapSORT/main.c
@@ -16,53 +16,67 @@ void constructHeap(int arr[], int n) {
     for (int i = n/2; i >= 0; i--) {
         temp = arr[i];
         j = i;
-        left = (2*j)+1;
-        right = (2*j)+2;
+        left = (2*j) + 1;
+        right = (2*j) + 2;
         while (left < n) {
-            if (right < n && arr[left] <= arr[right]) {
+            if (right < n && arr[left] >= arr[right]) {
                 imsi = right;
             }
             else {
                 imsi = left;
             }
-            if (temp>arr[imsi]) {
+            if (temp < arr[imsi]) {
                 break;
             }
-            arr[j]=arr[imsi];
+            arr[j] = arr[imsi];
             j = imsi;
-            left = 2 * j + 1;
-            right = 2 * j + 2;
+            left = (2 * j) + 1;
+            right = (2 * j) + 2;
         }
         arr[j] = temp;
     }
 }
 
 int heapsort(int arr[], int n) {
-    constructHeap(arr,n);
     for (int i = n - 1; i > 0; i--) {
-        swap(&arr[0],&arr[i]);
+        swap(&arr[0], &arr[i]);
         constructHeap(arr, i);
     }
+    return 0;
 }
 
 int main() {
     FILE *fin  = fopen("data.txt", "r");
-    FILE *fout = fopen("output.txt", "w");
+    FILE *fout = fopen("result.out", "w");
 
     int arr[1000];
     int n = 0;
+
     while (n < 1000 && fscanf(fin, "%d", &arr[n]) == 1) {
         n++;
     }
     fclose(fin);
+
+    fprintf(fout, "최초 배열 \n");
+    for (int i = 0; i < n; i++) {
+        fprintf(fout, "%d \n", arr[i]);
+    }
+    fprintf(fout, "\n");
+
+    constructHeap(arr, n);
+    fprintf(fout, "힙을 구성한 직후 배열 상태\n");
+    for (int i = 0; i < n; i++) {
+        fprintf(fout, "%d \n", arr[i]);
+    }
+    fprintf(fout, "\n");
     heapsort(arr, n);
 
-
+    fprintf(fout, " 힙 정렬 결과\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\n",arr[i]);
-        fprintf(fout, "%d\n", arr[i]);
+        fprintf(fout, "%d \n", arr[i]);
     }
-    fclose(fout);
+    fprintf(fout, "\n");
 
+    fclose(fout);
     return 0;
 }
