f9c8cb80799ed8500ec4bc7f921e75641a58de85
kang01hyoun <gus7618@gmail.com>
2025-06-07 02:07:00 +0900

    DFS 과제 추가

---
+++ DFS/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+
+typedef struct Node {
+    int vertex;
+    struct Node *nextNode;
+} Node;
+
+Node *head[N];
+int visited[N];
+Node *current[N];
+int stackarr[N];
+
+Node* newNode(int vertex){
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->vertex = vertex;
+    temp->nextNode = NULL;
+    return temp;
+}
+
+void addEdge(int from, int to){
+    Node *toNode = newNode(to);
+    toNode->nextNode = head[from];
+    head[from] = toNode;
+
+    Node *fromNode = newNode(from);
+    fromNode->nextNode = head[to];
+    head[to] = fromNode;
+}
+
+void DFS(int start, FILE *fout){
+    int top = -1;
+    visited[start] = 1;
+    fprintf(fout, "visited vertex: %d\n", start);
+    stackarr[++top] = start;
+
+    while (top >= 0) {
+        int curr = stackarr[top];
+
+        while (current[curr] != NULL && visited[current[curr]->vertex] == 1) {
+            current[curr] = current[curr]->nextNode;
+        }
+
+        if (current[curr] != NULL) {
+            int neighbor = current[curr]->vertex;
+            fprintf(fout, "from %d to %d visited\n", curr, neighbor);
+            visited[neighbor] = 1;
+            stackarr[++top] = neighbor;
+            current[curr] = current[curr]->nextNode;
+        }
+        else {
+            top--;
+        }
+    }
+    fprintf(fout, "\n\n");
+}
+
+int main(){
+    int starts[10];
+    int input;
+    int n = N;
+    int m;
+    FILE *fin, *fout;
+
+    fin = fopen("data.txt", "r");
+    fout = fopen("output.txt", "w");
+    fscanf(fin, "%d", &m);
+    for (int i = 0; i < 10; i++) {
+        head[i] = NULL;
+        visited[i] = 0;
+        current[i] = NULL;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int from;
+        int to;
+        fscanf(fin, "%d %d", &from, &to);
+        addEdge(from, to);
+    }
+
+    fprintf(fout, "인접리스트 출력\n");
+    for (int i = 0; i < 10; i++) {
+        fprintf(fout, "vertex %d", i);
+        Node *ptr = head[i];
+        while (ptr != NULL) {
+            fprintf(fout, " -> %d", ptr->vertex);
+            ptr = ptr->nextNode;
+        }
+        fprintf(fout, " -> NULL\n");
+    }
+
+
+    printf("탐색할 정점의 수를 입력하세요 : ");
+    scanf("%d", &input);
+    for (int i = 0; i < input; i++) {
+        printf("\n시작할 정점을 입력하십시오 : ");
+        scanf("%d", &starts[i]);
+    }
+
+    for (int index = 0; index < input; index++) {
+        int startv = starts[index];
+        for (int i = 0; i < 10; i++) {
+            visited[i] = 0;
+            current[i] = head[i];
+        }
+        fprintf(fout, "DFS 탐색 시작점 %d \n", startv);
+        DFS(startv, fout);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        Node *ptr = head[i];
+        while (ptr) {
+            Node *temp = ptr;
+            ptr = ptr->nextNode;
+            free(temp);
+        }
+    }
+    fclose(fout);
+    fclose(fin);
+    return 0;
+}
